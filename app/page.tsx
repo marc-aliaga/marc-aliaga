dea170ae18a8b97f842a77b64f368900a76430c0
markh4ck <marcaliagaborras@gmail.com>
2026-06-03 11:33:39 -0700

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const links = [
   {
-    title: "Articles",
+    title: "Notes from Eurasia",
     href: "https://medium.com/@marcaliagaborras",
     icon: (
       <svg viewBox="0 0 24 24" className="w-5 h-5 fill-current">
@@ -13,7 +13,7 @@ const links = [
   },
   {
     title: "Quant Projects",
-    href: "https://github.com/marc-aliaga?tab=repositories",
+    href: "https://drive.google.com/drive/folders/1TmvOOlJIKsT7H5DHjyTpBrYPP0uY4S4L?usp=sharing",
     icon: (
       <svg viewBox="0 0 24 24" className="w-5 h-5 fill-current">
         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
@@ -23,11 +23,6 @@ const links = [
   {
     title: "Valuations",
     href: "https://drive.google.com/drive/folders/1jz7B5NEn4niZ8868bzHenOJf8L9V1wuv?usp=sharing",
-    icon: (
-      <svg viewBox="0 0 24 24" className="w-5 h-5 fill-current">
-        <path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z" />
-      </svg>
-    ),
   },
   {
     title: "The Revaluation of Bitcoin Miners: Right Time, Right Place",
